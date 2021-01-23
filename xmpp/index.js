0b57997fe7f709951a06a7ee145c3a9c97017e1e
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2021-01-23 00:43:41 -0500

    Update index.js

--- xmpp/index.js
+++ xmpp/index.js
@@ -10,38 +10,7 @@ const wss = new WebSocket.Server({ port: process.env.xmppPort || 80 });
 wss.on("connection", ws => {
     var client = new Client(ws) 
 
-    ws.on("close", async (lol) => {
-        var friends = await Friends.findOne({id: client.id})
-
-        if (friends) {
-            friends.accepted.forEach(friend => {
-                if (xmppClients[friend.id]) {
-                    xmppClients[friend.id].client.ws.send(xmlbuilder.create({
-                        'presence': {
-                            '@xmlns': 'jabber:client',
-                            '@to': xmppClients[friend.id].client.jid,
-                            '@from': client.jid,
-                            '@type': "unavailable",
-                            'status': {
-                                "#text": {
-                                    "bHasVoiceSupport":false,
-                                    "bIsJoinable":false,
-                                    "bIsPlaying":false,
-                                    "Properties": {
-                                        "bInPrivate": true
-                                    },
-                                    "SessionId":"","Status":"Playing Battle Royale - 1 / 16"}
-                            }
-                        }
-                    }).end().replace(`<?xml version="1.0"?>`, "").trim())            
-                }
-            })
-                
-        }
-
-        if (client.sender) {
-            clearInterval(client.sender)
-        }
+    ws.on("close", async (dick) => {
         if (xmppClients[client.id]) delete xmppClients[client.id]
     })
 })
