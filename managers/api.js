7a19cda7cdc5b48eca8c53fa5b0cab4540807100
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2021-02-08 20:05:23 -0500

    Keychain..

--- managers/api.js
+++ managers/api.js
@@ -253,8 +253,10 @@ module.exports = (app) => {
 
 	//keychain
 	app.get('/fortnite/api/storefront/v2/keychain', (req, res) => {
-		axios.get("https://api.nitestats.com/v1/epic/keychain").then(response => {
+		axios.get("https://api.nitestats.com/v1/epic/keychain", {timeout: 3000}).then(response => {
 			res.json(response.data);
+		}).catch(e => {
+			res.json(["74AF07F9A2908BB2C32C9B07BC998560:V0Oqo/JGdPq3K1fX3JQRzwjCQMK7bV4QoyqQQFsIf0k=:Glider_ID_158_Hairy"])
 		})
 	})
 
