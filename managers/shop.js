c7b54e655393d6829e9d1068456e8863b3f24b0c
Beat-YT <justjdupuis@gmail.com>
2021-05-24 12:55:03 -0400

    Some change

--- managers/shop.js
+++ managers/shop.js
@@ -1,10 +1,29 @@
-const axios = require('axios');
+const axios = require('axios').default;
+Date.prototype.addHours = function (h) {
+  this.setTime(this.getTime() + (h * 60 * 60 * 1000));
+  return this;
+}
 
 module.exports = (app) => {
-    app.get('/fortnite/api/storefront/v2/catalog', (req, res) => {
-        axios.get("https://api.nitestats.com/v1/epic/store").then(response => {
-            let data = response.data
-            res.json(data)
-        })
+  app.get('/fortnite/api/storefront/v2/catalog', (req, res) => {
+    axios.get("https://api.nitestats.com/v1/epic/store", { timeout: 3000 }).then(response => {
+      res.json(response.data)
+    }).catch(err => {
+      res.json({
+        "refreshIntervalHrs": 24,
+        "dailyPurchaseHrs": 24,
+        "expiration": new Date().addHours(24),
+        "storefronts": [
+          {
+            "name": "BRDailyStorefront",
+            "catalogEntries": []
+          },
+          {
+            "name": "BRWeeklyStorefront",
+            "catalogEntries": []
+          }
+        ]
+      })
     })
+  })
 }
