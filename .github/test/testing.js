c27b0b8fde6c0238af6fb35037640f0034cd9fbc
Beat YT <66485277+Beat-YT@users.noreply.github.com>
2021-06-30 16:23:53 -0400

    Update testing.js

--- .github/test/testing.js
+++ .github/test/testing.js
@@ -7,7 +7,8 @@ module.exports = (app) => {
     const ExcludedEndpoint = [
         '/account/api/oauth/token',
         '/fortnite/api/game/v2/profile/:accountId/client/:command',
-        '/launcher/api/public/distributionpoints/'
+        '/launcher/api/public/distributionpoints/',
+        '/fortnite/api/game/v2/matchmakingservice/ticket/player/:accountId'
     ]
 
     const util = require('util')
