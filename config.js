c744daaefc0840f6a4957bb48c98f17f6c1869f0
Milxnor <84999745+Milxnor@users.noreply.github.com>
2021-07-21 02:05:21 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    botCid: "CID_286_Athena_Commando_F_NeonCat",
+    botPlatform: "WIN"
+    // TODO: Add more stuff idk lol
+}
+
+/*
+Windows = WIN
+Mac = MAC
+Xbox = XBL
+PlayStation = PSN
+Switch = SWT
+iOS = IOS
+Android = AND
+*/
