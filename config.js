7d02bf02e1d7d68f00b7611f90104aa9623947a4
Milxnor <84999745+Milxnor@users.noreply.github.com>
2021-07-21 19:27:29 +0000

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 module.exports = {
-    botCid: "CID_286_Athena_Commando_F_NeonCat",
-    botPlatform: "WIN"
+    botCid: "CID_A_141_Athena_Commando_M_Dojo", // Get the newest CIDS from https://sourceful.us/doc/311/fortnite-br-skin-ids--meshes-by-blithe
+    botPlatform: "XBL",
+    botUser: "NeoniteBot",
+    botStatus: "Neonite Bot",
+    botLevel: 100 
     // TODO: Add more stuff idk lol
 }
 
