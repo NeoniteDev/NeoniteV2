227fcd2a3d2512269a25810ad91ce722b5654250
Joshua <68912574+HybridFNBR@users.noreply.github.com>
2021-10-11 11:12:01 +0100

    Added Gold

--- managers/api.js
+++ managers/api.js
@@ -1040,7 +1040,15 @@ module.exports = (app) => {
 		res.json([])
 	})
 
-};
+};  
+    //change the 0 to what ever amount of gold you want
+    app.get('/fortnite/api/game/v2/br-inventory/account/:accountId', (req, res) => {
+	    res.json({
+		    "stash": {
+			    "globalcash": 0 
+		    },
+	    })
+    });
 
 
 function RandomString(length) {
